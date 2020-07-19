37ccf954378bdb7b8bdb9895100ef58ee72b9368
KELLY454545 <kimpopico10@gmail.com>
2020-07-19 13:40:30 +0400

    all

--- fancy_fonctions.sh
+++ fancy_fonctions.sh
@@ -1,1 +1,11 @@
-
+function gco ()
+{
+         if [[ -e  .git ]]
+         then 
+                        echo " vous etes sur la branche $(git status)"
+                        git commit -m $* []
+          else
+        
+                         echo "vous n'etes pas dans un projet git"
+           fi
+}
